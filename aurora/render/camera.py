1a2c713cb209d119b458f52f3169c6126b525daf
TagCopperLight <tagcopperlight@gmail.com>
2023-01-07 00:04:38 +0100

    zoom edit

--- aurora/render/camera.py
+++ aurora/render/camera.py
@@ -5,7 +5,7 @@ class Camera:
     def __init__(self, position: Vector2, screen_size: Vector2) -> None:
         self.position = position
         self.screen_size = screen_size
-        self.zoom = 50
+        self.zoom = 1
 
     def convert_position(self, position: Vector2) -> Vector2:
         return (position - self.position) * self.zoom + self.screen_size / 2

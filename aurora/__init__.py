ddfe79914aa954584a9c6b43c6c143ecfa5fd590
TagCopperLight <tagcopperlight@gmail.com>
2025-06-23 21:06:02 +0200

    cleanup

--- aurora/__init__.py
+++
@@ -1,2 +0,0 @@
-__author__ = "tagcopperlight"
-__version__ = "1.0.0"
--- aurora/entity.py
+++
@@ -1,9 +0,0 @@
-from .render.texture import Texture
-
-from gravity.math.vector import Vector2
-
-
-class RenderedEntity:
-    def __init__(self, position: Vector2, texture: Texture) -> None:
-        self.position = position
-        self.texture = texture

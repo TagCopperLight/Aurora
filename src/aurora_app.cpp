5672d9d946e906f34c9cdc29a2f7b4743c79a692
TagCopperLight <tagcopperlight@gmail.com>
2025-06-25 16:38:48 +0200

    push constants

--- src/aurora_app.cpp
+++ src/aurora_app.cpp
@@ -1,11 +1,20 @@
 #include "aurora_app.hpp"
 
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
 #include <stdexcept>
 #include <array>
 #include <spdlog/spdlog.h>
 #include <cassert>
 
 namespace aurora {
+    struct PushConstantsData {
+        glm::vec2 offset;
+        alignas(16) glm::vec3 color;
+    };
+
     AuroraApp::AuroraApp() {
         spdlog::debug("Initializing Aurora Application");
         loadModels();
@@ -64,12 +73,18 @@ namespace aurora {
 
     void AuroraApp::createPipelineLayout() {
         spdlog::debug("Creating pipeline layout");
+
+        VkPushConstantRange pushConstantRange{};
+        pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+        pushConstantRange.offset = 0;
+        pushConstantRange.size = sizeof(PushConstantsData);
+
         VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
         pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
         pipelineLayoutInfo.setLayoutCount = 0;
         pipelineLayoutInfo.pSetLayouts = nullptr;
-        pipelineLayoutInfo.pushConstantRangeCount = 0;
-        pipelineLayoutInfo.pPushConstantRanges = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 1;
+        pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
 
         if (vkCreatePipelineLayout(auroraDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
             throw std::runtime_error("Failed to create pipeline layout");
@@ -139,6 +154,9 @@ namespace aurora {
     }
 
     void AuroraApp::recordCommandBuffer(int imageIndex) {
+        static int frame = 0;
+        frame = (frame + 1) % 1000;
+
         VkCommandBufferBeginInfo beginInfo{};
         beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
         
@@ -155,7 +173,7 @@ namespace aurora {
         renderPassInfo.renderArea.extent = auroraSwapChain->getSwapChainExtent();
 
         std::array<VkClearValue, 2> clearValues{};
-        clearValues[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+        clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
         clearValues[1].depthStencil = {1.0f, 0};
         renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
         renderPassInfo.pClearValues = clearValues.data();
@@ -175,7 +193,17 @@ namespace aurora {
 
         auroraPipeline->bind(commandBuffers[imageIndex]);
         auroraModel->bind(commandBuffers[imageIndex]);
-        auroraModel->draw(commandBuffers[imageIndex]);
+
+        for (int j = 0; j < 4; j++) {
+            PushConstantsData push{};
+            push.offset = {-0.5f + frame * 0.001f, -0.4f + j * 0.25f};
+            push.color = {0.0f, 0.0f, 0.2f + j * 0.2f};
+            
+            vkCmdPushConstants(commandBuffers[imageIndex], pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(PushConstantsData), &push);
+            
+            auroraModel->draw(commandBuffers[imageIndex]);
+        }
+
 
         vkCmdEndRenderPass(commandBuffers[imageIndex]);
         if (vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS) {

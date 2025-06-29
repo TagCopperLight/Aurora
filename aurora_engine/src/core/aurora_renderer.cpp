4d939bca8c30f5b1f1255186e65fd1e6c6ae91bb
TagCopperLight <tagcopperlight@gmail.com>
2025-06-29 14:38:02 +0200

    Color Space and background

--- aurora_engine/src/core/aurora_renderer.cpp
+++ aurora_engine/src/core/aurora_renderer.cpp
@@ -125,7 +125,7 @@ namespace aurora {
         renderPassInfo.renderArea.extent = auroraSwapChain->getSwapChainExtent();
 
         std::array<VkClearValue, 2> clearValues{};
-        clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
+        clearValues[0].color = {0.314f, 0.314f, 0.314f, 1.0f};
         clearValues[1].depthStencil = {1.0f, 0};
         renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
         renderPassInfo.pClearValues = clearValues.data();

fd7c744f6f9dac8f7717662b6085a2d0caa7fe38
TagCopperLight <tagcopperlight@gmail.com>
2025-07-03 18:35:05 +0200

    Staging buffer

--- aurora_app/src/graphics/aurora_model.cpp
+++ aurora_app/src/graphics/aurora_model.cpp
@@ -24,18 +24,35 @@ namespace aurora {
         vertexCount = static_cast<uint32_t>(vertices.size());
         assert(vertexCount >= 3 && "Vertex count must be at least 3");
         VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
+
+        VkBuffer stagingBuffer;
+        VkDeviceMemory stagingBufferMemory;
+
         auroraDevice.createBuffer(
             bufferSize,
-            VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+            VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+            stagingBuffer,
+            stagingBufferMemory
+        );
+
+        void *data;
+        vkMapMemory(auroraDevice.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+        memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+        vkUnmapMemory(auroraDevice.device(), stagingBufferMemory);
+
+        auroraDevice.createBuffer(
+            bufferSize,
+            VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
             vertexBuffer,
             vertexBufferMemory
         );
 
-        void *data;
-        vkMapMemory(auroraDevice.device(), vertexBufferMemory, 0, bufferSize, 0, &data);
-        memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
-        vkUnmapMemory(auroraDevice.device(), vertexBufferMemory);
+        auroraDevice.copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+
+        vkDestroyBuffer(auroraDevice.device(), stagingBuffer, nullptr);
+        vkFreeMemory(auroraDevice.device(), stagingBufferMemory, nullptr);
     }
 
     void AuroraModel::createIndexBuffers(const std::vector<uint32_t> &indices) {
@@ -47,18 +64,35 @@ namespace aurora {
         }
         
         VkDeviceSize bufferSize = sizeof(indices[0]) * indexCount;
+        
+        VkBuffer stagingBuffer;
+        VkDeviceMemory stagingBufferMemory;
+
         auroraDevice.createBuffer(
             bufferSize,
-            VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+            VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+            stagingBuffer,
+            stagingBufferMemory
+        );
+
+        void *data;
+        vkMapMemory(auroraDevice.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+        memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
+        vkUnmapMemory(auroraDevice.device(), stagingBufferMemory);
+
+        auroraDevice.createBuffer(
+            bufferSize,
+            VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
             indexBuffer,
             indexBufferMemory
         );
 
-        void *data;
-        vkMapMemory(auroraDevice.device(), indexBufferMemory, 0, bufferSize, 0, &data);
-        memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
-        vkUnmapMemory(auroraDevice.device(), indexBufferMemory);
+        auroraDevice.copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+        vkDestroyBuffer(auroraDevice.device(), stagingBuffer, nullptr);
+        vkFreeMemory(auroraDevice.device(), stagingBufferMemory, nullptr);
     }
 
     void AuroraModel::draw(VkCommandBuffer commandBuffer) {
